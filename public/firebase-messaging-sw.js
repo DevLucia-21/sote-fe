5ec4543b5705bee4843dbfaac976e68e556d5a7e
dik <hienyang1207@gmail.com>
2025-11-14 21:22:39 +0900

    setting api connect

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js");
+
+// Firebase Config
+firebase.initializeApp({
+  apiKey: "AIzaSyAu_4_ANgwFOnTFyodDuDpUb6xQO_XEM9w",
+  authDomain: "sote-push.firebaseapp.com",
+  projectId: "sote-push",
+  messagingSenderId: "363644394694",
+  appId: "1:363644394694:web:22678a90fb1ca9653a8bc7"
+});
+
+// 백그라운드 알림 처리
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("📩 백그라운드 메시지 수신:", payload);
+
+  const notificationTitle = payload.notification.title || "새 알림";
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
